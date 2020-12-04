853c7a0514c85daed3b5b596d294ce7019a69962
Jerson Satoru Uyekita <jersonsatoru@yahoo.com.br>
2020-12-04 17:12:32 +0000

    Create Genre resource

--- app/Http/Controllers/Api/GenreController.php
+++ app/Http/Controllers/Api/GenreController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Genre;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\CategoryRequest;
-use App\Models\Category;
 use Illuminate\Http\Request;
 
-class CategoryController extends Controller
+class GenreController extends Controller
 {
 
     private $rules = [
@@ -17,30 +16,30 @@ class CategoryController extends Controller
 
     public function index()
     {
-        return Category::all();
+        return Genre::all();
     }
 
     public function store(Request $request)
     {
         $this->validate($request, $this->rules);
-        return Category::create($request->all());
+        return Genre::create($request->all());
     }
 
-    public function show(Category $category)
+    public function show(Genre $genre)
     {
-        return $category;
+        return $genre;
     }
 
-    public function update(Request $request, Category $category)
+    public function update(Request $request, Genre $genre)
     {
         $this->validate($request, $this->rules);
-        $category->update($request->all());
-        return $category;
+        $genre->update($request->all());
+        return $genre;
     }
 
-    public function destroy(Category $category)
+    public function destroy(Genre $genre)
     {
-        $category->delete();
+        $genre->delete();
         return response()->noContent();
     }
 }
